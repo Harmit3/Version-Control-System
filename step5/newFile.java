c5fd518637c2499932e9c3160a5dfd346fab914b
Harmit Patel <harmitpatel110@gmail.com>
2025-12-04 20:41:22 -0500

    all steps works perfectly and mapping is done.

---
+++ step5/newFile.java
@@ -0,0 +1,12 @@
+public class newFile{
+     public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+        // lines 5–7
+        if (x > 5 
+                && y > 15 
+                && (x + y) < 40) {
+            System.out.println("Condition true");
+        }
+    }
+}
---
+++ step5/oldFile.java
@@ -0,0 +1,10 @@
+public class oldFile{
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+        // line 5
+        if (x > 5 && y > 15 && (x + y) < 40) {
+            System.out.println("Condition true");
+        }
+    }
+}
